d1b257fea2b5833e2a784ffc76976f999bfac149
anil-ninja <kumar.anil8892@gmail.com>
2016-05-19 19:59:51 +0530

    final

--- ajax/dbconnect.php
+++ ajax/dbconnect.php
@@ -2,7 +2,7 @@
 	session_start();
 	$config['host'] = "localhost" ;
 	$config['user'] = "root" ;
-	$config['password'] = "ishu" ;
+	$config['password'] = "redhat@11111p" ;
 	$config['database'] = "survey" ;
 	$db_handle = mysqli_connect($config['host'], $config['user'], $config['password'], $config['database']);
 ?>
