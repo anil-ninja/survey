bed1f0906687f86cd802ec923372ce617a63e648
anil-ninja <kumar.anil8892@gmail.com>
2016-05-23 18:05:43 +0530

    email

--- ajax/email.php
+++ ajax/email.php
@@ -15,7 +15,7 @@
 		$email = $_POST['email']; 
 		$name = $_POST['name'];
 		$emailMessage = "Dear ".$name.",<br/><br/>
-Thank you for your valuable time to make survey happen.<br/>
+Thank you for your valuable time to complete the survey.<br/>
 To get relief from domestic helper on leave.<br/>
 Book Now on BlueTeam: <br/>
 <a href='http://goo.gl/545wov' target='_blank' >BlueTeam App</a><br/> or <img src='images/phone.png' data-original=''/> / <img src='images/whatsapp.png' data-original=''/>  +91 95990 75355 <br/><br/>
